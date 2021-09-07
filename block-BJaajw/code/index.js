84182b81d7e35696cbd977acaa29e72323511bc1
Vineet <vineet98love@gmail.com>
2021-09-08 00:15:44 +0530

    block-BJaajw

---
+++ block-BJaajw/code/index.js
@@ -0,0 +1,55 @@
+let url = 'https://api.spaceflightnewsapi.net/v3/articles?_limit=30/hghhs';
+
+let data = fetch(url).then((res) => {
+           if(!res.ok){
+              throw new Error(`Error happened: ${res.status}`);
+           }
+           return res.json();
+        });
+
+let display = document.querySelector('.display');
+
+let source = document.querySelector('.source');
+
+function handleChange(event) {
+  console.log(event);
+  document.querySelector('.display').innerHTML = '';
+
+  data.then((userData) =>
+    userData.forEach((data) => {
+      console.log(data.newsSite, event.target.value);
+      if (data.newsSite == event.target.value) {
+        createUI(data);
+      }
+    })
+  );
+}
+source.addEventListener('change', handleChange);
+
+data.then((userData) => userData.forEach((data) => createUI(data))).catch((error) => {
+    display.innerText = error;
+});
+
+function createUI(arg) {
+  console.log(arg);
+
+  let div = document.createElement('div');
+  let aside = document.createElement('aside');
+  let articleImage = document.createElement('img');
+  let article = document.createElement('article');
+  let categoryPara = document.createElement('p');
+  let articleHeading = document.createElement('h3');
+  let readMore = document.createElement('a');
+
+  div.setAttribute('class', 'flex');
+  readMore.innerText = 'READ MORE';
+  articleImage.src = arg.imageUrl;
+  articleHeading = arg.title;
+  categoryPara.innerText = arg.newsSite;
+  readMore.href = arg.url;
+
+  aside.append(articleImage);
+  article.append(categoryPara, articleHeading, readMore);
+  div.append(aside, article);
+  display.append(div);
+}
